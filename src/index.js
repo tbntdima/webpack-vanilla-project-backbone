bf8b275c7f19662bd9a1c7d90bc13c318632258c
Dmitriy An <an.dmitriy01@gmail.com>
2018-12-08 21:56:47 +0100

    improved index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 /**
- * Importing styles
- */
+ * Default Libraries
+ * If you don't need some libraries, remove unnesessary imports and that's it.
+ * -------------------------------------------------------------------------- */
+// Importing styles
 import './styles.scss';
 
-/**
- * Importing GSAP tools
- */
- import {TweenMax, CSSPlugin, ScrollToPlugin} from "gsap/all";
- // CSSPlugin and AttrPlugin may get dropped by webpack, so it's important
- // to keep below lines
+// Importing GSAP tools
+import {TweenMax, CSSPlugin, ScrollToPlugin} from "gsap/all";
+// CSSPlugin and AttrPlugin may get dropped by webpack, so it's important
+// to keep below lines if you use one of these Classes
 const plugins = [CSSPlugin, ScrollToPlugin];
 
-/**
- * Importing Scroll Magic
- */
+// Importing Scroll Magic
 import ScrollMagic from "ScrollMagic";
 import 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js';
+/* -------------------------------------------------------------------------- */
 
+
+/* Start coding here ---------------------------------------------------------*/
 (() => {
     // GreenSock sample
     const tween = TweenMax.to('#webpack-img', 6, {
@@ -26,3 +27,4 @@ import 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js';
         rotation: 360,
     });
 })();
+/* -------------------------------------------------------------------------- */
