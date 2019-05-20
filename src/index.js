89f8a804833b9aa3b12be53f0f914c34d2f0ccdc
Dmitriy An <an.dmitriy01@gmail.com>
2019-05-20 23:57:26 +0200

    simplify structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-/**
- * Default Libraries
- * If you don't need some libraries, remove unnesessary imports and that's it.
- * -------------------------------------------------------------------------- */
-// Importing styles
-import './styles.scss';
-
+import './scss/index.scss';
 // GSAP + ScrollMagic sample
 import './js/gsap-scrollmagic-sample.js';
-/* -------------------------------------------------------------------------- */
 
-/* Start coding here ---------------------------------------------------------*/
 console.log('hello world!');
-/* -------------------------------------------------------------------------- */
